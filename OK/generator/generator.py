d539662dd6aa77312bbd8b6c4b1c1ca42f311bb0
Hoaqin <michaldomasik@wp.pl>
2022-11-14 12:23:37 +0100

    update

---
+++ OK/generator/generator.py
@@ -0,0 +1,21 @@
+import random
+from pathlib import Path
+
+print("podaj ilość punktów")
+ilosc = int(input())
+print("podaj przedział z którgo chcesz otzrymac wsp. punktów")
+x = int(input())
+y = int(input())
+filepath = r"testy_2.txt"
+f = open(filepath, "w+")
+
+f.write(str(ilosc))
+f.write("\n")
+for i in range(0,ilosc):
+    x1 = random.randint(x, y)
+    y1 = random.randint(x, y)
+    f.write(str(x1))
+    f.write(" ")
+    f.write(str(y1))
+    f.write("\n")
+print(Path.cwd())
